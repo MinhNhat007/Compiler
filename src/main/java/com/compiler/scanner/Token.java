fe05581465a2cd0eb8cddc7fd3b378a42fad397a
MinhNhat007 <minhnhattrinh1995@gmail.com>
2018-04-18 23:10:14 +0200

    update equals and hashCode functions for Token

--- src/main/java/com/compiler/scanner/Token.java
+++ src/main/java/com/compiler/scanner/Token.java
@@ -55,6 +55,34 @@ public class Token {
   public void setContent(Content content) {
     this.content = content;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((content == null) ? 0 : content.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Token other = (Token) obj;
+    if (content == null) {
+      if (other.content != null)
+        return false;
+    } else if (!content.equals(other.content))
+      return false;
+    if (name != other.name)
+      return false;
+    return true;
+  }
   
 }
 
